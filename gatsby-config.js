cfdd1bf889a7473f466e137a5bf1f7ec0801322d
Amanda Ng <amandang19@gmail.com>
2020-05-17 16:07:15 +0800

    Create coming soon page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AMANDA NG`,
+    description: `Personal site of Amanda Ng. View contact info.`,
+    author: `@amanduhsays`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1d1d1f`,
+        theme_color: `#1d1d1f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-coming-soon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import WelcomeScreen from "../components/welcome-screen"
+import logo from "../images/logo.svg"
+
+import styles from "./index.module.css"
 
 const IndexPage = () => (
-  <Layout>
+  <div className={styles.page}>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <WelcomeScreen />
+    <div className={styles.hero}>
+      <img src={logo} className={styles.logo} />
+      <p className={styles.text}>Site coming soon...</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
